bea02213d75d07be7196d04eddb6ccbb7284831f
thaydonet <thaydo.net@gmail.com>
2026-04-20 10:01:14 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,22 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import cloudflare from '@astrojs/cloudflare';
 
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://sach.booktoan.com', // Thay đổi domain này thành domain thật của bạn
-  integrations: [react(), mdx({
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  }), tailwind(), sitemap()],
+  site: 'https://sach.booktoan.com',
+  output: 'server',
+  adapter: cloudflare(),
+  integrations: [
+    react(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    tailwind(),
+    sitemap()
+  ],
 });
